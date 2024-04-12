fe25668e35e0408cf7d332652ee3fca714ef9336
umitotuki <umitukiuk@gmail.com>
2024-04-12 07:36:02 +0000

    [finish]4-8

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2024_04_12_063133) do
+ActiveRecord::Schema.define(version: 2024_04_12_070534) do
+
+  create_table "post_images", force: :cascade do |t|
+    t.string "shop_name"
+    t.text "caption"
+    t.integer "user_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
