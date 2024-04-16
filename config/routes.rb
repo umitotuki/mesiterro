85f0b4dd85dbcf53ac56f8a692d276b0b365b075
umitotuki <umitukiuk@gmail.com>
2024-04-16 05:15:51 +0000

    a

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,6 @@ Rails.application.routes.draw do
    resources :post_images, only: [:new, :create, :index, :show, :destroy] do
       resource :favorite, only: [:create, :destroy]
       resources :post_comments, only: [:create, :destroy]
-   end 
+   end
    resources :users, only: [:show, :edit, :update]
   end
